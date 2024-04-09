bcfd8aaa7edda24b4d21a7e13fa95fcfa9a8d411
zenya2602 <zenya.konyaev@mail.ru>
2024-04-09 22:14:10 +0300

    #29 add thymeleaf config and test page

---
+++ src/main/kotlin/com/example/application/config/ThymeleafConfig.kt
@@ -0,0 +1,47 @@
+package com.example.application.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.ViewResolver
+import org.thymeleaf.spring5.SpringTemplateEngine
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
+import org.thymeleaf.spring5.view.ThymeleafViewResolver
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.ITemplateResolver
+
+
+@Configuration
+class ThymeleafConfig {
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    @Bean
+    fun thymeleafViewResolver(): ViewResolver {
+        val resolver = ThymeleafViewResolver()
+        resolver.templateEngine = thymeleafTemplateEngine()
+        resolver.characterEncoding = "UTF-8"
+        return resolver
+    }
+
+    @Bean
+    fun thymeleafTemplateEngine(): SpringTemplateEngine {
+        val engine = SpringTemplateEngine()
+        engine.enableSpringELCompiler = true
+        engine.setTemplateResolver(thymeleafTemplateResolver())
+        return engine
+    }
+
+    @Bean
+    fun thymeleafTemplateResolver(): ITemplateResolver {
+        val resolver = SpringResourceTemplateResolver()
+        resolver.setApplicationContext(applicationContext)
+        resolver.prefix = "classpath:/templates/"
+        resolver.suffix = ".html"
+        resolver.templateMode = TemplateMode.HTML
+        resolver.characterEncoding = "UTF-8"
+        resolver.isCacheable = false
+        return resolver
+    }
+}
